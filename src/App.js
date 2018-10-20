7443e0a5ef484a1a476cef0d89f7f595ac7e3765
Ketan Thakkar <Ketan_Thakkar@epam.com>
2018-10-20 15:17:16 +0200

    Added new features

--- src/App.js
+++ src/App.js
@@ -5,16 +5,23 @@ import Grid from './Grid/Grid';
 import Rocket from './Rocket/Rocket';
 import Smoke from './Rocket/Smoke';
 
-//modules
+import facts from './facts.json';
+
 import cssStyles from './First.module.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      facts
+    };
+  }
   render() {
     return (
       <div className={cssStyles.app}>
         <Title />
         <Background />
-        <Grid />
+        <Grid facts={this.state.facts} />
         <Rocket />
         <Smoke />
       </div>
--- src/Grid/GridItem.js
+++ src/Grid/GridItem.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import './GridItem.css';
 
-export default class GridItem extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>GridItem</h1>
-            </div>
-        );
-    }
+const GridItem = (props) => {
+    return (
+        <div className='card'>
+            <h1 className='title'>{props.fact.title}</h1>
+            <article className='fact'>{props.fact.fact}</article>
+        </div>
+    )
 }
+
+export default GridItem;
+
+
